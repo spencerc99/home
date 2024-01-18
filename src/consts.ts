a35ed7edc6b610925ed553f6219e18d85d18bd7a
Spencer Chang <spencerc99@gmail.com>
2024-01-18 15:48:18 -0800

    woah big first one, moved over home page, friends, and fits (almost)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "𝚜𝚙𝚎𝚗𝚌𝚎𝚛𝚌𝚑𝚊𝚗𝚐.𝚖𝚎 𝚒𝚜 𝚠𝚊𝚗𝚍𝚎𝚛𝚒𝚗𝚐";
+export const SITE_DESCRIPTION =
+  "pencer is a programmer, researcher, artist, and world maker in San Francisco. He creates playful and intimate software and words to imagine alternative futures of computing.";
---
+++ src/scripts/helpers.ts
@@ -0,0 +1,29 @@
+function showSnackbar(text) {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+  x.innerHTML = text;
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 2000);
+}
+
+function showLinkCopiedSnackbar() {
+  showSnackbar("Link copied to clipboard!");
+}
+
+function copyLink(text) {
+  navigator.clipboard.writeText(text);
+  showLinkCopiedSnackbar();
+  return undefined;
+}
+
+function scrollUp(elementId) {
+  document
+    .querySelector(`#${elementId}`)
+    ?.scrollIntoView({ behavior: "smooth" });
+}
