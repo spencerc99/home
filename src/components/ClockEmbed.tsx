d8a299c9559bdf3756bb55e9e295595c527e8f32
Spencer Chang <spencerc99@gmail.com>
2025-09-04 17:14:31 -0700

    checkpoint sidebar working

--- src/components/ClockEmbed.tsx
+++ src/components/ClockEmbed.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Footnote } from "./Footnote";
 
-export function ClockEmbed() {
+export function ClockEmbed({ size = 200 }) {
   return (
     <div id="clockEmbed">
       <a href="https://clock.spencer.place" className="button">
@@ -9,7 +9,10 @@ export function ClockEmbed() {
           caption="an embed of the clock that I made that changes colors with the time such that every minute has a unique color."
           asChild={true}
         >
-          <iframe src="https://clock.spencer.place"></iframe>
+          <iframe
+            src="https://clock.spencer.place"
+            style={{ maxWidth: size, maxHeight: size }}
+          ></iframe>
         </Footnote>
       </a>
     </div>
