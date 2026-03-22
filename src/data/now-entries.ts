47bfc3a7baec4563bb3fd76b06e1cccc9d99f621
Spencer Chang <spencerc99@gmail.com>
2026-03-22 19:45:13 -0400

    update now & collab

--- src/data/now-entries.ts
+++ src/data/now-entries.ts
@@ -9,6 +9,17 @@ export interface NowEntry {
 
 export const nowEntries: NowEntry[] = [
   {
+    date: "2026-03-22",
+    items: [
+      'Exploring making art with my <a href="/creation/internet-movement">internet debris</a>',
+      'Making a <a href="/creation/we-were-online">game for the internet</a> filled with <a href="/creation/playhtml">tiny social networks</a>',
+      "Collecting boulders to put in a park",
+      'Shaping culture through <a href=\"https://www.instagram.com/spence.r.chang/\">social</a> <a href=\"https://x.com/spencerc99\">media</a>.',
+      "Exploring how to develop <a href='https://www.are.na/spencer-chang/textures-lnz8erkbwe'>patina</a> on various materials",
+      'Open to <a href="/collab">invitations & collaborations</a> for teaching, crafting, and scheming.',
+    ],
+  },
+  {
     date: "2026-02-14",
     items: [
       'Exploring making art with my <a href="https://x.com/spencerc99/status/2013758318016451065">internet debris</a>',
